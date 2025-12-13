062289d7bc88d0e2776f5f076cd4224c13a12ed9
sebastiandunn <sebastiancdunn@gmail.com>
2025-12-12 22:01:18 -0700

    feat: add MainPage with adaptive layout for phone/tablet

---
+++ src/RoMoteNoAds/Views/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+using RoMoteNoAds.Controls;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RoMoteNoAds.Views;
+
+public partial class MainPage : ContentPage
+{
+    private readonly IServiceProvider _serviceProvider;
+    private ContentPage? _currentPage;
+    private string _currentTab = "remote";
+
+    public string CurrentTab
+    {
+        get => _currentTab;
+        set
+        {
+            if (_currentTab != value)
+            {
+                _currentTab = value;
+                OnPropertyChanged();
+                NavigateToTab(value);
+            }
+        }
+    }
+
+    public MainPage(IServiceProvider serviceProvider)
+    {
+        InitializeComponent();
+        _serviceProvider = serviceProvider;
+        BindingContext = this;
+
+        // Setup layout based on device type
+        SetupLayout();
+
+        // Navigate to initial tab
+        NavigateToTab("remote");
+    }
+
+    private void SetupLayout()
+    {
+        var isTablet = DeviceInfo.Idiom == DeviceIdiom.Tablet ||
+                       DeviceInfo.Idiom == DeviceIdiom.Desktop;
+
+        if (isTablet)
+        {
+            // Tablet: Sidebar on left, content on right
+            RootGrid.ColumnDefinitions.Clear();
+            RootGrid.ColumnDefinitions.Add(new ColumnDefinition(new GridLength(100)));
+            RootGrid.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Star));
+
+            Sidebar.IsVisible = true;
+            TabBar.IsVisible = false;
+
+            Grid.SetColumn(Sidebar, 0);
+            Grid.SetColumn(ContentArea, 1);
+        }
+        else
+        {
+            // Phone: Content with bottom tab bar
+            RootGrid.ColumnDefinitions.Clear();
+            RootGrid.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Star));
+
+            Sidebar.IsVisible = false;
+            TabBar.IsVisible = true;
+
+            Grid.SetColumn(ContentArea, 0);
+        }
+    }
+
+    private void OnSidebarItemSelected(object? sender, string tabId)
+    {
+        CurrentTab = tabId;
+    }
+
+    private void OnTabSelected(object? sender, string tabId)
+    {
+        CurrentTab = tabId;
+    }
+
+    private void NavigateToTab(string tabId)
+    {
+        ContentPage? newPage = tabId switch
+        {
+            "remote" => _serviceProvider.GetService<RemotePage>(),
+            "channels" => _serviceProvider.GetService<ChannelsPage>(),
+            "shortcuts" => _serviceProvider.GetService<ShortcutsPage>(),
+            _ => null
+        };
+
+        if (newPage != null && newPage != _currentPage)
+        {
+            _currentPage = newPage;
+            PageContent.Content = newPage.Content;
+        }
+    }
+
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+
+        // Re-evaluate layout on size change (orientation, window resize)
+        SetupLayout();
+    }
+}
